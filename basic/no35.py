d394b9aaeac6cf042d7a6631e359eb9156cf7383
MINYUZA <sncr_crmn@naver.com>
2021-11-07 02:42:24 +0900

    DAY_35

--- basic/no35.py
+++ basic/no35.py
@@ -1,9 +1,15 @@
 # BOJ 단계별로 풀어보기
 # 3. for문
 
-# 8393
-n = int(input())
-result = 0
-for i in range(n+1):
-    result += i
-print(result)
+# 15552
+import sys
+
+input = sys.stdin.readline
+t = int(input())
+num_list = []
+for i in range(t):
+    a, b = map(int, input().split())
+    num_list.append(a + b)
+
+for i in range(t):
+    print(num_list[i])
